f3169273fc70f30f2f7687637e0ce14363850d05
Stephmdavis1 <125842540+Stephmdavis1@users.noreply.github.com>
2023-02-19 22:05:43 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import requests
 
 API_KEY = "9e60f0a5c6ceda58c06cbddea08fce3e"
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
 request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
@@ -10,10 +10,9 @@ response = requests.get(request_url)
 if response.status_code == 200:
     data = response.json()
     weather = data['weather'][0]['description']
-    print(weather)
     temperature = round(data['main']['temp'] - 273.15, 2)
     
     print("Weather:", weather)
-    print("temperature:", temperature, "celsius")
+    print("Temperature:", temperature, "celsius")
 else:
     print("An error occurred.")
